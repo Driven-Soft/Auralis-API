7a54510babdb0957beec74e7654bd415427ba8ca
Felipe Beatriz <felipe.beatriz199@gmail.com>
2025-11-17 18:16:36 -0300

    fix: adição do ambiente de desenvolvimento na permissão CORS

--- src/main/java/br/com/fiap/resource/CorsFilter.java
+++ src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -6,6 +6,8 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 import jakarta.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
+import java.util.Arrays;
+
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
@@ -13,7 +15,11 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-        headers.add("Access-Control-Allow-Origin", "https://auralis-gs.vercel.app");
+        String origin = requestContext.getHeaderString("Origin");
+        String[] allowed = new String[]{"https://auralis-gs.vercel.app", "http://localhost:5173"};
+        if (origin != null && Arrays.asList(allowed).contains(origin)) {
+            headers.add("Access-Control-Allow-Origin", origin);
+        }
         headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         headers.add("Access-Control-Allow-Credentials", "true");
         headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
